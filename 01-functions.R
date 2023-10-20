797cabc58cd315fb8a72829d38056bff9fa0a2bf
lollex90 <j.m.terlikowski@lse.ac.uk>
2023-10-20 15:03:16 +0100

    add the option to change lbs to kg

--- 01-functions.R
+++ 01-functions.R
@@ -1,13 +1,22 @@
 # this function works for the mtcars dataset
-manipulate_data <- function(data){
+manipulate_data <- function(data, change_to_kg = F){
   "
   Compute the weight to gross horsepower ratio
     data: a dataframe with columns called wt and hp corresponding
       to the car's weight and gross horsepower
+    change_to_kg: bool indicating whether to change the units from lbs to kg, 
+      default = False
   "
   
   data_manipulated <- data %>% 
     mutate(weight_per_hp = wt/hp)
+  
+  # change weight units to kg
+  if(change_to_kg == T){
+    data_manipulated <- data_manipulated %>% 
+      mutate(wt = wt*0.4536,
+             weight_per_hp = weight_per_hp*0.4536)
+  }
   
   return(data_manipulated)
 }
